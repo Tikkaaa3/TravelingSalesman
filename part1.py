369dc6d4799ea228fd630aa7e890f1681ccf0beb
Emre Tolga Kaptan <112636626+Tikkaaa3@users.noreply.github.com>
2024-11-18 22:21:03 +0100

    distance function added

--- part1.py
+++ part1.py
@@ -1,8 +1,9 @@
-#pandas for storing data from tsp
-#numpy for storing route
+# pandas for storing data from tsp
+# numpy for storing route
 
 import numpy as np
 import pandas as pd
+import math
 
 
 def parser(file_name):
@@ -30,7 +31,22 @@ def parser(file_name):
     return cities
 
 
-file_name = "cities/berlin11_modified.tsp"  # Path to the city file
+def distance(city1, city2):
+    x1 = city1["x_location"]
+    x2 = city2["x_location"]
+    y1 = city1["y_location"]
+    y2 = city2["y_location"]
+    d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return d
+
+
+file1 = "cities/berlin11_modified.tsp"
+cities_df = parser(file1)
+
+print(cities_df)
+two_cities_distance = distance(cities_df.loc[0], cities_df.loc[1])
+print(two_cities_distance)
+
 cities_df = parser(file_name)  # Call the parser function
 
 # Print the resulting DataFrame
