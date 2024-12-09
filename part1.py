5acb4ab3534e9139b6a79ef9585ac291fc571e77
Emre Tolga Kaptan <112636626+Tikkaaa3@users.noreply.github.com>
2024-12-09 17:58:52 +0100

    Updated to part2

--- part1.py
+++ part1.py
@@ -28,26 +28,71 @@ def parser(file_name):
                     cities.append([int(node), float(x), float(y)])
 
     cities = pd.DataFrame(cities, columns=["Node", "x_location", "y_location"])
+    cities.set_index("Node", inplace=True)
     return cities
 
 
 def distance(city1, city2):
-    x1 = city1["x_location"]
-    x2 = city2["x_location"]
-    y1 = city1["y_location"]
-    y2 = city2["y_location"]
+    x1, y1 = city1["x_location"], city1["y_location"]
+    x2, y2 = city2["x_location"], city2["y_location"]
     d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return d
 
 
+def fitness_function(solution, cities):
+    res = 0
+    for i in range(0, len(solution) - 1):
+        res += distance(cities.loc[solution[i]], cities.loc[solution[i+1]])
+    res += distance(cities.loc[solution[-1]], cities.loc[solution[0]])
+    return res
+
+
+def info(solution, cities):
+    score = fitness_function(solution, cities)
+    print(solution)
+    print(score)
+
+
+def greedy(cities, start):
+    solution = np.array([start])
+    cities_list = [i for i in range(1, len(cities)+1)]
+    city = start
+    cities_list.remove(city)
+    while len(cities) > 0:
+        shortest = float('inf')
+        for node in cities_list:
+            if node not in solution:
+                dist = distance(cities.loc[solution[-1]], cities.loc[node])
+                if dist < shortest:
+                    shortest = dist
+                    city = node
+        solution = np.append(solution, city)
+        cities_list.remove(city)
+        if not cities_list:
+            solution = np.append(solution, start)
+            break
+    return solution
+
+
 file1 = "cities/berlin11_modified.tsp"
+# file1 = "cities/berlin52.tsp"
 cities_df = parser(file1)
+# print(cities_df)
 
-print(cities_df)
-two_cities_distance = distance(cities_df.loc[0], cities_df.loc[1])
-print(two_cities_distance)
+city_nodes = np.arange(1, len(cities_df) + 1)
+np.random.shuffle(city_nodes)
+solution = city_nodes
 
-cities_df = parser(file_name)  # Call the parser function
+result = fitness_function(solution, cities_df)
 
-# Print the resulting DataFrame
-print(cities_df)
+# info(solution, cities_df)
+# 5474.001848763631
+
+info(greedy(cities_df, 1), cities_df)
+# 4543.086327880018
+
+# 8182.1915557256725
+# berlin52 best w greedy
+
+# for i in cities_df.index:
+#     info(greedy(cities_df, i), cities_df)
