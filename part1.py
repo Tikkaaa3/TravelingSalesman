539053bcca82ce1d1ea4f7391e9489293b85f118
Emre Tolga Kaptan <112636626+Tikkaaa3@users.noreply.github.com>
2024-12-12 18:30:26 +0100

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,5 +1,5 @@
 # pandas for storing data from tsp
-# numpy for storing route
+# numpy for storing solutions, population
 
 import numpy as np
 import pandas as pd
@@ -8,26 +8,11 @@ import travelingsalesman as tsp
 
 
 file1 = "cities/berlin11_modified.tsp"
-# file1 = "cities/berlin52.tsp"
 cities_df = tsp.parser(file1)
-# print(cities_df)
 
-city_nodes = np.arange(1, len(cities_df) + 1)
-np.random.shuffle(city_nodes)
-solution = city_nodes
 
-result = tsp.fitness_function(solution, cities_df)
+tsp.solution_info(tsp.greedy(cities_df, 1), cities_df)
 
-# info(solution, cities_df)
-# 5474.001848763631
 
-tsp.info(tsp.greedy(cities_df, 1), cities_df)
-# 4543.086327880018
-
-# 8182.1915557256725
-# berlin52 best w greedy
-
-# for i in cities_df.index:
-#     info(greedy(cities_df, i), cities_df)
-
-# implement little bit elite(%5) and much more tournament or roulette(%95) cycle crossover is better to use
+population = tsp.initial_population(cities_df)
+tsp.population_info(population, cities_df)
